50c24481b35e7cdd070946f3e62b636af68260e5
Evan Weinberg <eweinberg@ssis.edu.vn>
2021-02-07 10:09:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ from sphero_rvr import *
 
 rvr = RVRDrive()
 time.sleep(2.0)
-rvr.drive_to_position_si(0.0,0.05,0.0,0.0)
+rvr.drive_to_position_si(0.0,0,0.8,0.5)
 time.sleep(2.0)
-rvr.drive_to_position_si(90.0,0.0,0.0,0.0)
+rvr.drive_to_position_si(90.0,1.6,0.8,0.5)
