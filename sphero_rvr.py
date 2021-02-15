23c20182d37a126872b3cbc0eedee9e47a63171e
Evan Weinberg <weinbergmath@gmail.com>
2021-02-15 19:01:12 +0700

    Update sphero_rvr.py
    Updated with LED and wake functions

--- sphero_rvr.py
+++ sphero_rvr.py
@@ -5,6 +5,18 @@ import struct
 
 uart = busio.UART(board.D2, board.D3, baudrate=115200)
 
+class LEDs:
+    RIGHT_HEADLIGHT = [0x00, 0x00, 0x00, 0x07]
+    LEFT_HEADLIGHT = [0x00, 0x00, 0x00, 0x38]
+    LEFT_STATUS = [0x00, 0x00, 0x01, 0xC0]
+    RIGHT_STATUS = [0x00, 0x00, 0x0E, 0x00]
+    BATTERY_DOOR_FRONT = [0x00, 0x03, 0x80, 0x00]
+    BATTERY_DOOR_REAR = [0x00, 0x00, 0x70, 0x00]
+    POWER_BUTTON_FRONT = [0x00, 0x1C, 0x00, 0x00]
+    POWER_BUTTON_REAR = [0x00, 0xE0, 0x00, 0x00]
+    LEFT_BRAKELIGHT = [0x07, 0x00, 0x00, 0x00]
+    RIGHT_BRAKELIGHT = [0x38, 0x00, 0x00, 0x00]
+
 class RawMotorModes:
     OFF = 0
     FORWARD = 1
@@ -64,7 +76,7 @@ class RVRDrive:
 
     def float_to_hex(self,f):
         #return hex(struct.unpack('<I', struct.pack('<f', f))[0])
-        result = bytearray(struct.pack('f', f))
+        result = bytearray(struct.pack('>f', f))
 
         return result
 
@@ -117,12 +129,36 @@ class RVRDrive:
         # Call raw motor function
         self.set_raw_motors(leftMode,left,rightMode,right)
 
-    @staticmethod
-    def reset_yaw():
+    
+    def reset_yaw(self):
         drive_data = [0x8D, 0x3E, 0x12, 0x01, 0x16, 0x06, 0x00]
 
         drive_data.extend([~((sum(drive_data) - 0x8D) % 256) & 0x00FF, 0xD8])
 
-        self.uart.write(bytearray(drive_data))
+        self._uart.write(bytearray(drive_data))
 
+        return
+        
+   
+    def set_all_leds(self, red, green, blue):
+        led_data = [
+            0x8D, 0x3E, 0x11, 0x01, 0x1A, 0x1A, 0x00,
+            0x3F, 0xFF, 0xFF, 0xFF
+        ]
+        for _ in range (10):
+            led_data.extend([red, green, blue])
+        led_data.extend([~((sum(led_data) - 0x8D) % 256) & 0x00FF, 0xD8])
+        self._uart.write(bytearray(led_data))
+        return
+        
+    def wake(self):
+        power_data = [0x8D, 0x3E, 0x11, 0x01, 0x13, 0x0D, 0x00]
+        power_data.extend([~((sum(power_data) - 0x8D) % 256) & 0x00FF, 0xD8])
+        self._uart.write(bytearray(power_data))
+        return
+    
+    def sleep(self):
+        power_data = [0x8D, 0x3E, 0x11, 0x01, 0x13, 0x01, 0x00]
+        power_data.extend([~((sum(power_data) - 0x8D) % 256) & 0x00FF, 0xD8])
+        self._uart.write(bytearray(power_data))
         return
