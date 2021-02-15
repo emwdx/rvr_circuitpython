92c8fe9d3203608f664cd2c1389bcab8eddf1058
Evan Weinberg <eweinberg@ssis.edu.vn>
2021-02-15 18:59:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ from sphero_rvr import *
 
 
 rvr = RVRDrive()
-time.sleep(2.0)
-rvr.drive_to_position_si(0.0,0,0.8,0.5)
-time.sleep(2.0)
+rvr.wake()
+rvr.reset_yaw()
+rvr.set_all_leds(255,0,0)
+time.sleep(5.0)
+rvr.drive_to_position_si(0.0,0.0,0.8,0.5)
+rvr.set_all_leds(0,255,0)
+time.sleep(5.0)
 rvr.drive_to_position_si(90.0,1.6,0.8,0.5)
+rvr.set_all_leds(0,0,255)
+time.sleep(5.0)
+rvr.set_all_leds(255,0,255)
