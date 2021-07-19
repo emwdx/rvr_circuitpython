fec0fcc5563ae3951335615a415a796da55a5252
Evan Weinberg <weinbergmath@gmail.com>
2021-07-19 14:48:07 +0700

    Update drive_to_position_annotated.py

--- drive_to_position_annotated.py
+++ drive_to_position_annotated.py
@@ -41,19 +41,19 @@ uart = busio.UART(board.TX, board.RX, baudrate=115200)
 def drive_to_position_si(yaw_angle, x, y, speed):
 
   # This sets up the list of bytes needed for this command to be sent to the RVR through the serial port.
-    SOP = 0x8d. #Always the start byte for a command to the RVR
+    SOP = 0x8d #Always the start byte for a command to the RVR
     FLAGS = 0x06 #This tells the RVR to ignore the target and source ID, and that this command expects a response only if there are errors.
     TARGET_ID = 0x0e
     SOURCE_ID = 0x0b
-    
+
   # The drive system ID is 0x16
     DEVICE_ID = 0x16
   # The drive_to_position_si command has ID of 38
     COMMAND_ID = 0x38
-    
+
   # The sequence value is arbitrarily 1. We aren't using the sequence capability here.
     SEQ = 0x01
-    
+
   # The final byte in the command is 0xD8
     EOP = 0xD8
 
@@ -65,10 +65,10 @@ def drive_to_position_si(yaw_angle, x, y, speed):
     speed = bytearray(struct.pack('>f', speed))
   # The command has a flags byte that lets you set different attributes for how the robot moves between the positions. Check out page 12 of the SpheroRVRControlSystem manual for the details.
     flags = bytearray(struct.pack('B', 0))
-    
+
   # Now we build the command packet byte by byte. First the first five bytes:
     output_packet = [SOP, FLAGS, DEVICE_ID,COMMAND_ID,SEQ]
-    
+
   # Now the bytes for the flags
     output_packet.extend(yaw_angle)
     output_packet.extend(x)
@@ -78,7 +78,7 @@ def drive_to_position_si(yaw_angle, x, y, speed):
   # And the checksum byte which does some math with the sum of the previous bytes in the command, and finally the end of packet byte.
     output_packet.extend([~((sum(output_packet) - SOP) % 256) & 0x00FF,EOP])
 
-  # Now that the command is complete, return the command as an array of bytes.  
+  # Now that the command is complete, return the command as an array of bytes.
     return bytearray(output_packet)
 
 # END OF DRIVE_TO_POSITION_SI
@@ -101,7 +101,7 @@ COORDINATES = [[0,0],[0,2],[-1,2],[-1,3],[2,3],[2,2],[0,1],[0,0],[0,0.1]]
 
 # The for-loop below turns the tile coordinates into real world positions in meters.
 positions = []
-for pair in COORDINATES
+for pair in COORDINATES:
     positions.append([0.0,pair[0]*TILE_WIDTH,pair[1]*TILE_WIDTH])
 
 # This for-loop iterates over the pairs of points in the list
@@ -110,17 +110,17 @@ for i in range(len(positions)-1):
     next_position = positions[i+1]
     dx = next_position[1] - current_position[1]
     dy = next_position[2] - current_position[2]
-    
+
     # This calculates a heading for the movement about to occur.
     raw_angle = -180.0/3.1415926*math.atan2(dx,dy)
-    
-    # This line 
+
+    # This line
     command = drive_to_position_si(raw_angle,next_position[1],next_position[2],SPEED)
     uart.write(command)
-    
+
     # This waits for the command to complete based on the distance to the next point, along with some helpful fudge factors.
     travel_time = math.sqrt(dx*dx + dy*dy)/SPEED*1.1 + 1.5
-    
+
     #This print statement is for debugging purposes.
     print("Driving to {0},{1}, heading {2}, travel time {3}".format(next_position[1],next_position[2], raw_angle,travel_time))
     time.sleep(travel_time)
